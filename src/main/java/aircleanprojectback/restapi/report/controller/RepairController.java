a7a8509717f99cc7a9e5481172aa835190f1bf58
doa <leedoa0819@gmail.com>
2024-07-16 17:17:30 +0900

    [24.07.16] 보고서 dto, entity

---
+++ src/main/java/aircleanprojectback/restapi/report/controller/RepairController.java
@@ -0,0 +1,5 @@
+package aircleanprojectback.restapi.report.controller;
+
+public class RepairController {
+    // 지점 시설물 수리보고서
+}
---
+++ src/main/java/aircleanprojectback/restapi/report/controller/VehicleRepairController.java
@@ -0,0 +1,5 @@
+package aircleanprojectback.restapi.report.controller;
+
+public class VehicleRepairController {
+    // 본사 차량 수리 보고서
+}
