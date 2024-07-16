f4fa9e802f9e782ae60f0b72507708df801eabc2
doa <leedoa0819@gmail.com>
2024-07-16 22:23:31 +0900

    [24.07.16] BranchSalesReport rest api

--- src/main/java/aircleanprojectback/restapi/report/controller/BranchSalesController.java
+++ src/main/java/aircleanprojectback/restapi/report/controller/BranchSalesController.java
@@ -2,13 +2,12 @@ package aircleanprojectback.restapi.report.controller;
 
 import aircleanprojectback.restapi.common.dto.ResponseDTO;
 import aircleanprojectback.restapi.report.dto.BranchSalesDTO;
+import aircleanprojectback.restapi.report.service.BranchSalesService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.sql.Date;
 import java.util.ArrayList;
@@ -19,19 +18,56 @@ import java.util.List;
 @Slf4j
 public class BranchSalesController {
     // 지점매출보고서
+
+    private final BranchSalesService branchSalesService;
+    public BranchSalesController(BranchSalesService branchSalesService) {this.branchSalesService = branchSalesService;}
+
     private List<BranchSalesDTO> branchSales = new ArrayList<>();
 
-    // 샘플 데이터 추가 (테스트용)
-    public BranchSalesController() {
-        branchSales.add(new BranchSalesDTO(1, Date.valueOf("2024-07-16"), 100000, "Submitted", "Ariel", "Downy", "Clorox", "Shout", "Affresh", "Bounce", "B001"));
-        branchSales.add(new BranchSalesDTO(2, Date.valueOf("2024-07-17"), 200000, "Submitted", "Tide", "Snuggle", "OxiClean", "Zout", "Lemi Shine", "Snuggle", "B002"));
-    }
 
+    // /company 지점지출보고서 전체 조회
     @GetMapping("/company/reports")
     public ResponseEntity<ResponseDTO> selectAllBranchSales() {
 
         return  ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "조회성공", branchSales));
     }
+
+    // /location 지점 지출 보고서 자신이 쓴 지출 보고서 전체 조회
+    @GetMapping("/location/{branchCode}/reports")
+    public ResponseEntity<ResponseDTO> selectBranchSalesByBranchCode(@PathVariable String branchCode) {
+
+
+        return  ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK,"조회성공",branchSales));
+    }
+
+    // /location 지점 지출보고서 작성
+    @PostMapping("/location/reports")
+    public ResponseEntity<ResponseDTO> insertBranchSales(@RequestBody BranchSalesDTO branchSalesDTO) {
+
+        return ResponseEntity.ok()
+                .body(new ResponseDTO(HttpStatus.OK,"보고서 등록 성공", branchSalesService.insertBranchSales(branchSalesDTO)));
+
+    }
+
+    // /location 지점 지출보고서 수정
+    @PutMapping("/location/reports/{branchReportCode}")
+    public ResponseEntity<ResponseDTO> updateBranchSales(@RequestBody BranchSalesDTO branchSalesDTO, @PathVariable int branchReportCode) {
+
+        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "수정에 성공하였습니다", branchSalesService.updateBranchSales(branchSalesDTO, branchReportCode)));
+    }
+
+    // /location 지점장이 지출보고서 삭제
+    @DeleteMapping("/location/reports/{branchReportCode}")
+    public ResponseEntity<ResponseDTO> deleteBranchSales(@PathVariable int branchReportCode) {
+
+        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "삭제에 성공하였습니다", branchSalesService.deleteBranchSales(branchReportCode)));
+
+    }
+
+
+
+
+
 
 
 }
