1a8ebf8e460498ec85b9dfa31214ac4c1588b705
LAPTOP-DSTQNHQ2\LN-36 <sciboviator@gmail.com>
2024-07-16 10:09:29 +0900

    [DB] DB 업데이트

--- src/main/java/aircleanprojectback/restapi/member/dto/MemberDTO.java
+++ src/main/java/aircleanprojectback/restapi/member/dto/MemberDTO.java
@@ -22,11 +22,11 @@ public class MemberDTO implements UserDetails {
     private String memberEmail;
     private String memberPhone;
     private String memberRole;
-    private String birthDate;
-    private String gender;
+    private String memberBirthDate;
+    private String memberGender;
     private String memberAddress;
     private String memberStatus;
-    private Date hireDate;
+    private Date memberHireDate;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
