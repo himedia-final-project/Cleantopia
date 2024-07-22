c373c9020cfa8e39cd80bbb1ded978135b399970
pyunghun <564564q@naver.com>
2024-07-22 20:34:16 +0900

    지점관리 지점 등록, 삭제

---
+++ src/main/java/aircleanprojectback/restapi/branch/Message/Message.java
@@ -0,0 +1,28 @@
+package aircleanprojectback.restapi.branch.Message;
+
+public class Message {
+
+    private int status;
+    private String message;
+
+    public Message(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
