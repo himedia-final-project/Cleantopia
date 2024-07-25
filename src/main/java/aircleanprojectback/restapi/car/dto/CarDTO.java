ca9439b0b286807d60fa87e94f7a441702055f52
choseungyoun <tmddbs9313@naver.com>
2024-07-25 10:34:31 +0900

    car entity

---
+++ src/main/java/aircleanprojectback/restapi/car/dto/CarDTO.java
@@ -0,0 +1,23 @@
+package aircleanprojectback.restapi.car.dto;
+
+import lombok.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CarDTO {
+
+    private String carNumber; // 차량번호
+    private String driverName; // 운전자 이름
+    private String carAssignedStatus; // 배정여부
+    private Date carDate; // 출고일
+    private String carPhoto; // 차량 사진
+    private String carEtc; // 특이사항
+
+
+}
+
