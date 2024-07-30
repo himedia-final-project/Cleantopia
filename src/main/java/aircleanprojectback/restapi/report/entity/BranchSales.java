787fa03f0464f8963ba4da47b88650e042c86078
doa <leedoa0819@gmail.com>
2024-07-30 17:52:28 +0900

    보고서관리 - 지점장매출보고서작성

--- src/main/java/aircleanprojectback/restapi/report/entity/BranchSales.java
+++ src/main/java/aircleanprojectback/restapi/report/entity/BranchSales.java
@@ -26,34 +26,44 @@ public class BranchSales {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd") // JSON 직렬화/역직렬화 형식 지정
     private Date branchSubmissionDate;      // 제출일
 
-    @Column(name = "offline_sales")
+    @Column(name = "offline_sales", nullable = true)
     private int officeSales;                // 오프라인매출
 
     @Column(name = "branch_report_status", length = 100, nullable = false)
     private String branchReportStatus = "접수";      // 보고서상태
 
-    @Column(name = "detergent", length = 200, nullable = false)
+    @Column(name = "detergent", length = 200, nullable = true)
     private String detergent;               // 세제
 
-    @Column(name = "fabric_softener", length = 200, nullable = false)
+    @Column(name = "fabric_softener", length = 200, nullable = true)
     private String fabricSoftener;          // 섬유유연제
 
-    @Column(name = "bleach", length = 200, nullable = false)
+    @Column(name = "bleach", length = 200, nullable = true)
     private String bleach;                  // 표백제
 
-    @Column(name = "stain_remover", length = 200, nullable = false)
+    @Column(name = "stain_remover", length = 200, nullable = true)
     private String stainRemover;            // 얼룩제거제
 
-    @Column(name = "washer_cleaner", length = 200, nullable = false)
+    @Column(name = "washer_cleaner", length = 200, nullable = true)
     private String washerCleaner;           // 세탁조 클리너
 
-    @Column(name = "dryer_sheet", length = 200, nullable = false)
+    @Column(name = "dryer_sheet", length = 200, nullable = true)
     private String dryerSheet;              // 건조기시트
 
-    @Column(name = "branch_code", length = 200, nullable = false)
+    @Column(name = "total_branch_sales_cost")
+    private int totalBranchSalesCost;      // 총지출금액
+
+    @Column(name = "member_name")
+    private String memberName;;             // 지점장명
+
+    @Column(name = "branch_name")
+    private String branchName;              // 지점명
+
+
+    @Column(name = "branch_code", length = 200, nullable = true)
     private String branchCode;              // 지점코드
 
-    @Column(name = "member_id")
+    @Column(name = "member_id", nullable = true)
     private String memberId;
 
     public BranchSales() {
@@ -112,6 +122,21 @@ public class BranchSales {
         return this;
    }
 
+   public  BranchSales totalBranchSalesCost(int totalBranchSalesCost) {
+        this.totalBranchSalesCost = totalBranchSalesCost;
+        return this;
+   }
+
+   public  BranchSales memberName(String memberName) {
+        this.memberName = memberName;
+        return this;
+   }
+
+   public  BranchSales branchName(String branchName) {
+        this.branchName = branchName;
+        return this;
+   }
+
    public  BranchSales branchCode(String branchCode) {
         this.branchCode = branchCode;
         return this;
@@ -123,7 +148,23 @@ public class BranchSales {
     }
 
    public BranchSales build() {
-        return new BranchSales(branchReportCode, branchSubmissionDate, officeSales, branchReportStatus, detergent, fabricSoftener, bleach, stainRemover, washerCleaner, dryerSheet, branchCode, memberId);
+        return new BranchSales(
+                branchReportCode,
+                branchSubmissionDate,
+                officeSales,
+                branchReportStatus,
+                detergent,
+                fabricSoftener,
+                bleach,
+                stainRemover,
+                washerCleaner,
+                dryerSheet,
+                totalBranchSalesCost,
+                memberName,
+                branchName,
+                branchCode,
+                memberId
+        );
    }
 
     public void setBranchReportStatus(String branchReportStatus) {
--- src/main/java/aircleanprojectback/restapi/report/repository/BranchSalesRepository.java
+++ src/main/java/aircleanprojectback/restapi/report/repository/BranchSalesRepository.java
@@ -1,15 +1,13 @@
 package aircleanprojectback.restapi.report.repository;
 
 
-import aircleanprojectback.restapi.report.dto.BranchAndMember;
 import aircleanprojectback.restapi.report.entity.BranchSales;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
 
 public interface BranchSalesRepository extends JpaRepository<BranchSales, Integer> {
 
-
+//    @Query(value = "SELECT m.member_name, b.branch_name FROM tbl_members m JOIN tbl_branch b ON m.member_id = b.member_id", nativeQuery = true)
+//    List<BranchAndMembersDTO> findBranchMember();
 
 }
