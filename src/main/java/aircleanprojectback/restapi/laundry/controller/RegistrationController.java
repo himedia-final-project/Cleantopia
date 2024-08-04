aea8d08c99842f8344743b91a32f3d8e8dca00e5
pyunghun <564564q@naver.com>
2024-08-04 21:43:40 +0900

    세탁방법 도출

--- src/main/java/aircleanprojectback/restapi/laundry/controller/RegistrationController.java
+++ src/main/java/aircleanprojectback/restapi/laundry/controller/RegistrationController.java
@@ -1,18 +1,40 @@
-//package aircleanprojectback.restapi.laundry.controller;
-//
-//
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//@RequestMapping("/registration")
-//public class RegistrationController {
-//
-//
-//    @GetMapping
-//
-//
-//
-//
-//}
+package aircleanprojectback.restapi.laundry.controller;
+
+
+import aircleanprojectback.restapi.laundry.Message.ResponseMessage;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.nio.charset.Charset;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/registration")
+public class RegistrationController {
+
+
+    @PostMapping("registLaundryWay")
+    public ResponseEntity<ResponseMessage> updateLaundryStatus(@RequestBody Map<String, Object> payload) {
+
+        System.out.println("여기를 확인해주세요");
+        System.out.println("payload 너 뭐야?"+payload);
+        
+
+        Map<String, Object> responseMap = new HashMap<>();
+//        responseMap.put("updateSuccess", updateSuccess);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(new ResponseMessage(200, "세탁 방법 도출 성공", responseMap));
+    }
+
+
+
+}
