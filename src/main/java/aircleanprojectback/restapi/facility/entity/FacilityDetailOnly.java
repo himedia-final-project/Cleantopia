2954d03a6ce0cff6cc6ae35d2bec917212a58f67
wjdals83 <tjwjdals0803@naver.com>
2024-08-06 12:41:39 +0900

    [FEATURE] facility CRUD  완성

---
+++ src/main/java/aircleanprojectback/restapi/facility/entity/FacilityDetailOnly.java
@@ -0,0 +1,29 @@
+package aircleanprojectback.restapi.facility.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="tbl_facility_detail")
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Getter
+@Builder(toBuilder = true)
+public class FacilityDetailOnly {
+
+    @Id
+    @Column(name="facility_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int facilityId;
+
+    @Column(name="branch_code")
+    private String branchCode;
+
+    @Column(name="facility_code")
+    private int facilityCode;
+
+    @Column(name="facility_status")
+    private String facilityStatus;
+
+}
--- src/main/java/aircleanprojectback/restapi/facility/repository/UpdatateWaterTanckRepository.java
+++ src/main/java/aircleanprojectback/restapi/facility/repository/UpdatateWaterTanckRepository.java
@@ -2,9 +2,7 @@ package aircleanprojectback.restapi.facility.repository;
 
 import aircleanprojectback.restapi.laundry.entity.WaterTank;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface UpdatateWaterTanckRepository extends JpaRepository<WaterTank, Integer> {
     WaterTank findByBranchCode(String branchCode);
 }
