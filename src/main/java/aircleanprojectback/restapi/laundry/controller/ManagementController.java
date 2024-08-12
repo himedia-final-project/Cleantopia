2b3f1df480b2ce273a3b3021549c4aaf0ed2275b
pyunghun <564564q@naver.com>
2024-08-12 19:38:08 +0900

    스웨거 등록

--- src/main/java/aircleanprojectback/restapi/laundry/controller/ManagementController.java
+++ src/main/java/aircleanprojectback/restapi/laundry/controller/ManagementController.java
@@ -6,6 +6,7 @@ import aircleanprojectback.restapi.laundry.dto.LaundryDTO;
 import aircleanprojectback.restapi.laundry.dto.WaterTankDTO;
 import aircleanprojectback.restapi.laundry.service.ManagementService;
 import aircleanprojectback.restapi.water.dto.WaterSupplyDTO;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -19,6 +20,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/management")
+@Tag(name = "세탁물 관리 API")
 public class ManagementController {
 
     private final ManagementService managementService;
@@ -27,97 +29,70 @@ public class ManagementController {
         this.managementService = managementService;
     }
 
+    @Tag(name = "수질 탱크 조회" ,description = "세탁물 및 수질 관리 관련 API")
     @GetMapping("/waterTank")
     public ResponseEntity<ResponseMessage> waterTank() {
-
         List<WaterTankDTO> waterTankList = managementService.waterTankList();
 
         Map<String, Object> responseMap = new HashMap<>();
         responseMap.put("waterTank", waterTankList);
 
-
-//        System.out.println("responseMap = " + responseMap);
-
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
 
-
         return ResponseEntity.ok()
                 .headers(headers)
-                .body(new ResponseMessage(200, "BranchList 조회 성공", responseMap));
+                .body(new ResponseMessage(200, "waterTank 조회 성공", responseMap));
     }
 
+    @Tag(name = "수질 일지 조회", description = "작성한 수질 일지 조회")
     @GetMapping("/waterSupply/{branchCode}")
     public ResponseEntity<ResponseMessage> waterSupply(@PathVariable String branchCode) {
-
         List<WaterSupplyDTO> waterSupplyList = managementService.SelectWaterSupply(branchCode);
 
         Map<String, Object> responseMap = new HashMap<>();
         responseMap.put("waterSupply", waterSupplyList);
 
-
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
 
-
         return ResponseEntity.ok()
                 .headers(headers)
-                .body(new ResponseMessage(200, "BranchList 조회 성공", responseMap));
+                .body(new ResponseMessage(200, "waterSupply 조회 성공", responseMap));
     }
 
+    @Tag(name = "세탁물 더미 조회", description = "고객에게 받은 세탁물 조회")
     @GetMapping("/selectLaundry/{branchCode}")
     public ResponseEntity<ResponseMessage> selectLaundry(@PathVariable String branchCode) {
-
-//        System.out.println("너 실행되니?");
-//        System.out.println(branchCode);
-//        List<WaterSupplyDTO> waterSupplyList = managementService.SelectWaterSupply(branchCode);
         List<LaundryDTO> selectLandryList = managementService.selectLaundry(branchCode);
 
         Map<String, Object> responseMap = new HashMap<>();
         responseMap.put("selectLandry", selectLandryList);
 
-//        System.out.println("여기확인");
-//        System.out.println(branchCode);
-
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(new MediaType("application", "json", Charset.forName("UTF-8")));
-
 
         return ResponseEntity.ok()
                 .headers(headers)
                 .body(new ResponseMessage(200, "laundry 조회 성공", responseMap));
     }
 
-    // 도착된 세탁 조회
+    @Tag(name = "도착한 세탁물 조회", description = "고객에게서 지점으로 도착한 세탁물 조회")
     @GetMapping("arrivedLaundry/{branchCode}")
-    public ResponseEntity<ResponseDTO> getLaundryArrived(@PathVariable String branchCode){
-
-        System.out.println("arrivedLaundry 실행됨" + branchCode );
-
+    public ResponseEntity<ResponseDTO> getLaundryArrived(@PathVariable String branchCode) {
         List<LaundryDTO> arrivedLaundryList = managementService.getLaundryArrived(branchCode);
 
-
-        
-        System.out.println("arrivedLaundryList = " + arrivedLaundryList);
-
-        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK,"도착한 세탁물 ",arrivedLaundryList));
+        return ResponseEntity.ok().body(new ResponseDTO(HttpStatus.OK, "도착한 세탁물", arrivedLaundryList));
     }
 
-
-    // 승낙 버튼 눌렀을때 동작
+    @Tag(name = "세탁물 상태 업데이트", description = "승낙 버튼 누를시 세탁 상태 변경")
     @PutMapping("/updateLaundryStatus")
     public ResponseEntity<ResponseMessage> updateLaundryStatus(@RequestBody Map<String, Object> payload) {
         int laundryCode = (int) payload.get("laundryCode");
         String statusType = (String) payload.get("statusType");
         String statusValue = (String) payload.get("statusValue");
 
-        System.out.println("Updating laundry status:");
-        System.out.println("Laundry Code: " + laundryCode);
-        System.out.println("Status Type: " + statusType);
-        System.out.println("Status Value: " + statusValue);
-
         boolean updateSuccess = managementService.updateLaundryStatus(laundryCode, statusType, statusValue);
-
 
         Map<String, Object> responseMap = new HashMap<>();
         responseMap.put("updateSuccess", updateSuccess);
@@ -134,11 +109,5 @@ public class ManagementController {
                     .headers(headers)
                     .body(new ResponseMessage(500, "laundry 상태 업데이트 실패", responseMap));
         }
-
     }
-
-
-
-
-
 }
